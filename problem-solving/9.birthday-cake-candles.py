4dcaa879e7aec5c954c509b1676da1a2d106fa55
yusuf <yusuftechie+git@gmail.com>
2023-11-13 17:19:16 +0530

    adding problems 7-10

---
+++ problem-solving/9.birthday-cake-candles.py
@@ -0,0 +1,36 @@
+# https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    max_value = max(candles)
+    count_max = candles.count(max_value)
+    return count_max
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
